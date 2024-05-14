710e4eb4b57cdc7d3772ad2c53757ef651318d9f
kamranafridi123 <kamranafridi089@gmail.com>
2024-05-14 20:16:28 +0100

    commited

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
 {
 	public function index()
 	{
-		$products = Product::all();
+		$products = Product::all()->sortByDesc("sku");;
 
  		return view('products.index', [
 			'products' => $products,
