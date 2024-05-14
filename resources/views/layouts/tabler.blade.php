8124ac3426bf98d45bb37c6dbb5441f8daf59fcb
kamranafridi123 <kamranafridi089@gmail.com>
2024-05-14 21:00:05 +0100

    commit

--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -322,50 +322,6 @@
 
 
 
-                                @if (auth()->user()->role == 'supplier')
-                                    <li class="nav-item dropdown {{ request()->is('orders*') ? 'active' : null }}">
-                                        <a class="nav-link dropdown-toggle" href="#navbar-base"
-                                            data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button"
-                                            aria-expanded="false">
-                                            <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="icon icon-tabler icon-tabler-package-export" width="24"
-                                                    height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                    stroke="currentColor" fill="none" stroke-linecap="round"
-                                                    stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M12 21l-8 -4.5v-9l8 -4.5l8 4.5v4.5" />
-                                                    <path d="M12 12l8 -4.5" />
-                                                    <path d="M12 12v9" />
-                                                    <path d="M12 12l-8 -4.5" />
-                                                    <path d="M15 18h7" />
-                                                    <path d="M19 15l3 3l-3 3" />
-                                                </svg>
-                                            </span>
-                                            <span class="nav-link-title">
-                                                {{ __('Orders') }}
-                                            </span>
-                                        </a>
-                                        <div class="dropdown-menu">
-                                            <div class="dropdown-menu-columns">
-                                                <div class="dropdown-menu-column">
-                                                    <a class="dropdown-item" href="{{ route('orders.index') }}">
-                                                        {{ __('All') }}
-                                                    </a>
-                                                    <a class="dropdown-item" href="{{ route('orders.complete') }}">
-                                                        {{ __('Completed') }}
-                                                    </a>
-                                                    <a class="dropdown-item" href="{{ route('orders.pending') }}">
-                                                        {{ __('Pending') }}
-                                                    </a>
-                                                    <a class="dropdown-item" href="{{ route('due.index') }}">
-                                                        {{ __('Due') }}
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                @endif
                                 @if (auth()->user()->role == 'admin')
                                     <li class="nav-item dropdown {{ request()->is('purchases*') ? 'active' : null }}">
                                         <a class="nav-link dropdown-toggle" href="#navbar-base"
