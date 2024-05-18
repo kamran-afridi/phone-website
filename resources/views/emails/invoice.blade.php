a4b9d5366ce1fd79a280edfcfb352fe4c4c0470e
kamranafridi123 <kamranafridi089@gmail.com>
2024-05-18 18:43:30 +0100

    commit

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -27,8 +27,8 @@
                                 <div class="col-lg-6 col-sm-6">
                                     <div class="logo">
                                         {{-- <h1>{{ Str::title(auth()->user()->name) }}</h1> --}}
-                                        <img src="{{ asset('assets/img/logo.PNG') }}" style="width: 125px; height:125px"
-                                            alt="Panther Force">
+                                        <img src="https://inventory.qtechltd.com/assets/img/logo.PNG"
+                                            style="width: 125px; height:125px" alt="Panther Force">
                                     </div>
                                 </div>
                                 <div class="col-lg-6 col-sm-6">
