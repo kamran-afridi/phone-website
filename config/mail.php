0aa4558840c6424b86e75929fbae6bb6bb34fc5c
kamranafridi123 <kamranafridi089@gmail.com>
2024-05-18 19:17:57 +0100

    COMMIT

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'url' => env('MAIL_URL'), 
             // 'host' => env('MAIL_HOST', '127.0.0.1'),
             // 'port' => env('MAIL_PORT', 2525),
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
             'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
