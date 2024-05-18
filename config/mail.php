df762be4b7d9a10f669410fd71c60abbfc6ea400
kamranafridi123 <kamranafridi089@gmail.com>
2024-05-18 20:52:15 +0100

    commit

--- config/mail.php
+++ config/mail.php
@@ -35,9 +35,10 @@ return [
 
     'mailers' => [
         'smtp' => [
-            'transport' => 'smtp',
+            'transport' => 'SMTP',
+            'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 465),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
