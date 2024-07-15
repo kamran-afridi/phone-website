d8e1d4cff0ae9b591612c4349d068c33cb35414b
kamranafridi123 <kamranafridi089@gmail.com>
2024-07-15 17:51:59 +0100

    commit

--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
+    <link rel="shortcut icon" href="{{ asset('fav.png') }}" type="image/x-icon" />
 
     <!-- CSS files -->
     <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
