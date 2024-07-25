0a7959744c71a5d45e9b860d9e857784d05dd81c
kamranafridi123 <kamranafridi089@gmail.com>
2024-07-25 17:29:55 +0100

    commit

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -105,7 +105,7 @@
                                     </svg>
                                     {{ __('Back') }}
                                 </a>
-                                <a class="btn btn-warning" href="{{ route('products.edit', $product->uuid) }}">
+                                <a class="btn btn-warning" href="{{ route('products.edit', $product->id) }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil"
                                         width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
                                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
