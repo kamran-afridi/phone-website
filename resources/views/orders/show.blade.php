570e7492f09f81d90db6dcd0f5b5aad8445853a1
kamranafridi123 <kamranafridi089@gmail.com>
2024-07-28 13:48:12 +0100

    commit

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -170,6 +170,19 @@
                                                     </svg>
                                                 </button>
                                             </form>
+                                            <button type="button" class="btn btn-sm mb-1 btn-primary"
+                                                data-bs-toggle="modal" data-bs-target="#myModal">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-pencil" width="24"
+                                                    height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4">
+                                                    </path>
+                                                    <path d="M13.5 6.5l4 4"></path>
+                                                </svg>
+                                            </button>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -241,7 +254,7 @@
                     <div class="modal-content">
                         <form action="{{ route('customers.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            <div class="row">   
+                            <div class="row">
                                 <div class="col-lg-12">
                                     <div class="card">
                                         <div class="card-body">
