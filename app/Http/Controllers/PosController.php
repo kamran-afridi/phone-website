a3544c72ca2a58431cc20b5561f80b50b630dee9
kamranafridi123 <kamranafridi089@gmail.com>
2024-08-08 13:17:46 +0100

    commit

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -26,7 +26,7 @@ class PosController extends Controller
 
 	public function addCartItem(Request $request)
 	{
-		// $request->all();
+		$request->all();
 		//dd($request);
 
 		$rules = [
@@ -40,7 +40,8 @@ class PosController extends Controller
 
 		Cart::add(
 			$validatedData['id'],
-			$validatedData['name'], 
+			$validatedData['name'],
+			1,
 			$validatedData['sale_price'], 
 			$validatedData['sku'],
 			1,
