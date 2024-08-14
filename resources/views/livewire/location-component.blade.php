060d74bc8d2478c703e1bc5bf16717e7fc700211
kamranafridi123 <kamranafridi089@gmail.com>
2024-08-14 01:44:43 +0100

    commit

--- resources/views/livewire/location-component.blade.php
+++ resources/views/livewire/location-component.blade.php
@@ -1,24 +1,22 @@
 <div>
     <select class="form-select form-control-solid @error('customer_id') is-invalid @enderror" id="customer_id"
         name="customer_id" wire:change="changeEvents($event.target.value)">
-        <option selected="" disabled="">
+        <option value="" selected="" disabled="">
             Select a customer:
         </option>
-
         @foreach ($customers as $customer)
-            <option value="{{ $customer->id }}">
+            <option value="{{ $customer->id }}" {{ old('customer_id') == $customer->id ? 'selected' : '' }}>
                 {{ $customer->name }}
             </option>
         @endforeach
     </select>
-    <div>
-        <p>Latitude: {{ $latitude }}</p>
-        <p>Longitude: {{ $longitude }}</p>
+    <div class="mt-3">
+        <p> <b>Latitude:</b> {{ $getlatitude }}</p>
+        <p> <b>Longitude:</b> {{ $getlongitude }}</p>
     </div>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-            // let map; 
-            function initializeMap(latitude, longitude) { 
+            function initializeMap(latitude, longitude) {
                 //     // console.log(position.coords.longitude);
                 var currentCenter = map.getZoom();
                 console.log(currentCenter);
@@ -26,10 +24,10 @@
                 zooMarker.setLatLng([latitude[0]['latitude'], latitude[0]['longitude']]);
                 //     zooMarker.setLatLng([latitude, longitude]);
             }
-
             Livewire.on('locationUpdated', (latitude, longitude) => {
                 initializeMap(latitude, longitude);
             });
+
         });
     </script>
 </div>
