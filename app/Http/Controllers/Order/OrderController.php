9cc57f584b96a39b6ad6d4efe7bdcba58546dc11
kamranafridi123 <kamranafridi089@gmail.com>
2024-08-17 15:42:02 +0100

    commit

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -57,8 +57,7 @@ class OrderController extends Controller
 	}
 
 	public function store(OrderStoreRequest $request)
-	{
-		dd($request->all());
+	{ 
 		$order = Order::create([
 			'customer_id' => $request->customer_id,
 			'payment_type' => $request->payment_type,
