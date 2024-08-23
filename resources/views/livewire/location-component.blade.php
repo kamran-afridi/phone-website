9fa92b24e35fbc104197329fc3df812ca9eeddae
kamranafridi123 <kamranafridi089@gmail.com>
2024-08-23 19:16:52 +0100

    commit

--- resources/views/livewire/location-component.blade.php
+++ resources/views/livewire/location-component.blade.php
@@ -14,38 +14,58 @@
     <div class="mt-3">
         <p> <b>Latitude:</b> {{ $getlatitude }}</p>
         <p> <b>Longitude:</b> {{ $getlongitude }}</p>
+        <input type="hidden" name="getlatitude" id="getlatitude" value="{{ $getlatitude }}">
+        <input type="hidden" name="getlongitude" id="getlongitude" value="{{ $getlongitude }}">
     </div>
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-
-            // Set an interval to trigger the changeEvents method every 500ms
-            // setInterval(function() {
-            var selectedCustomerId = document.getElementById('customer_id').value;
-
-            if (selectedCustomerId) {
-                // Trigger the Livewire component's changeEvents method
-                // Livewire.on('changeEvents', selectedCustomerId);
-                // Livewire.on('locationUpdated', selectedCustomerId);
-                // Livewire.on('locationUpdated', (latitude, longitude) => {
-                //     initializeMap(latitude, longitude);
-                // });
-                console.log(selectedCustomerId);
-            }
-            // }, 100000); 
-            function initializeMap(latitude, longitude) {
-                //     // console.log(position.coords.longitude);
-                var currentCenter = map.getZoom();
-                console.log(currentCenter);
-                map.setView([latitude[0]['latitude'], latitude[0]['longitude']], currentCenter);
-                zooMarker.setLatLng([latitude[0]['latitude'], latitude[0]['longitude']]);
-                //     zooMarker.setLatLng([latitude, longitude]);
-            }
-            Livewire.on('locationUpdated', (latitude, longitude) => {
-                initializeMap(latitude, longitude);
-            });
-            // setInterval(function() {
-            //     @this.$refresh(); 
-            // }, 120000); // 120000 milliseconds = 2 minutes
+        $('#customer_id').on('change', function() {
+            setInterval(function() {
+                var selectedCustomerId = document.getElementById('customer_id').value;
+                let getlatitude = document.getElementById('getlatitude').value;
+                let getlongitude = document.getElementById('getlongitude').value;
+                if (selectedCustomerId) {
+                    Livewire.dispatch('getlocation', {
+                        customer_id: selectedCustomerId,
+                    });
+                    if (getlongitude == 'Not detected') {
+                        getlongitude = 0.000;
+                        getlatitude = -0.000;
+                    }
+                    initializeMap(getlatitude, getlongitude);
+                    console.log(selectedCustomerId);
+                    // console.log(getlatitude, getlongitude);
+                }
+            }, 20000);
         });
+        // document.addEventListener('DOMContentLoaded', function() {
+        //  wire:change="changeEvents($event.target.value)"
+        // Set an interval to trigger the changeEvents method every 500ms
+        // setInterval(function() {
+        // var selectedCustomerId = document.getElementById('customer_id').value; 
+        // if (selectedCustomerId) {
+        // Trigger the Livewire component's changeEvents method
+        // Livewire.on('changeEvents', selectedCustomerId);
+        // Livewire.on('locationUpdated', selectedCustomerId);
+        // Livewire.on('locationUpdated', (latitude, longitude) => {
+        //     initializeMap(latitude, longitude);
+        // });
+        // console.log(selectedCustomerId);
+        // }
+        // }, 100000); 
+        function initializeMap(getlatitude, getlongitude) {
+            console.log(getlatitude + " " + getlongitude);
+            var currentCenter = map.getZoom();
+            // console.log(currentCenter);
+            map.setView([getlatitude, getlongitude], currentCenter);
+            zooMarker.setLatLng([getlatitude, getlongitude]);
+            zooMarker.setLatLng([getlatitude, getlongitude]);
+        }
+        // Livewire.on('locationUpdated', (latitude, longitude) => {
+        // initializeMap(latitude, longitude);
+        // });
+        // setInterval(function() {
+        //     @this.$refresh(); 
+        // }, 120000); // 120000 milliseconds = 2 minutes
+        // });
     </script>
 </div>
