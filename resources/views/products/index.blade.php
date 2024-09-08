128d08f99cb4c7b2d59d98dc9847de4d4ca461f4
kamranafridi123 <kamranafridi089@gmail.com>
2024-09-08 23:16:15 +0100

    add csv file action

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,15 +6,16 @@
             <x-empty title="No products found" message="Try adjusting your search or filter to find what you're looking for."
                 button_label="{{ __('Add your first Product') }}" button_route="{{ route('products.create') }}" />
 
-            {{-- <div style="text-center" style="padding-top:-25px">
+            <div style="text-center" style="padding-top:-25px">
                 <center>
                     <a href="{{ route('products.import.view') }}" class="">
                         {{ __('Import Products') }}
                     </a>
                 </center>
-            </div> --}}
+            </div>
         @else
             <div class="container-xl">
+                
                 <x-alert />
                 @livewire('tables.product-table')
             </div>
