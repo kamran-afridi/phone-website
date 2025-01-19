1f3b5b000cb6a851f668627fd57fd6c345f20bdc
mistermsami <mistermsami@gmail.com>
2025-01-20 04:16:17 +0500

    changes in invoice (bank details)

--- resources/views/orders/admin-print-invoice.blade.php
+++ resources/views/orders/admin-print-invoice.blade.php
@@ -39,13 +39,53 @@
                                         <h1>
                                             Invoice # <span>{{ $order->invoice_no }}</span>
                                         </h1>
-                                        <div class="invoice-number d-flex mt-3">
-                                            <h4 class="inv-title-1">
-                                                Invoice date:&nbsp;
-                                            </h4>
-                                            <p class="invo-addr-1">
-                                                {{ $order->order_date }}
-                                            </p>
+                                        <div class="invoice_details">
+                                            <div class="invoice-number d-flex mt-3">
+                                                <h4 class="inv-title-1">
+                                                    Invoice date:&nbsp;
+                                                </h4>
+                                                <p class="invo-addr-1">
+                                                    {{ $order->order_date }}
+                                                </p>
+                                            </div>
+                                            <div class="invoice-number d-flex mt-0">
+                                                <h4 class="inv-title-1">
+                                                    Bank Name:&nbsp;
+                                                </h4>
+                                                <p class="invo-addr-1">
+                                                    LLOYDS BANK
+                                                </p>
+                                            </div>
+                                            <div class="invoice-number d-flex mt-0">
+                                                <h4 class="inv-title-1">
+                                                    Bank Title:&nbsp;
+                                                </h4>
+                                                <p class="invo-addr-1">
+                                                    PANTHER FORCE RETAIL LIMITED
+                                                </p>
+                                            </div>
+                                            <div class="invoice-number d-flex mt-0">
+                                                <h4 class="inv-title-1">
+                                                    Sort Code:&nbsp;
+                                                </h4>
+                                                <p class="invo-addr-1">
+                                                    30-99-50
+                                                </p>&nbsp; &nbsp;
+                                                <h4 class="inv-title-1">
+                                                    Account No:&nbsp;
+                                                </h4>
+                                                <p class="invo-addr-1">
+                                                    50539663
+                                                </p>
+                                            </div>
+                                            {{-- <div class="invoice-number d-flex mt-0">
+                                                <h4 class="inv-title-1">
+                                                    Account No:&nbsp;
+                                                </h4>
+                                                <p class="invo-addr-1">
+                                                    50539663
+                                                </p>
+                                            </div> --}}
                                         </div>
                                     </div>
                                 </div>
