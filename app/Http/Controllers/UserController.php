7fb84c11c3ed3c8c3346193dde387f1e59b5b465
kamranafridi123 <kamranafridi089@gmail.com>
2025-07-26 01:30:25 +0100

    Add debug statement to store method for user creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
     public function store(StoreUserRequest $request)
     {
         if (Auth::user()->role == 'superAdmin') {
-            // dd($request->all());
+            dd($request->all());
             $user = User::create([
                 'name'  => $request->name,
                 'uuid' => Str::uuid(),
