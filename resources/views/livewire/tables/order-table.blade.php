08e44c21242827598f7da70a14f368266f8c1cac
kamran afridi <kamranafridi089@gmail.com>
2025-09-15 12:44:11 +0100

    commit

--- resources/views/livewire/tables/order-table.blade.php
+++ resources/views/livewire/tables/order-table.blade.php
@@ -6,35 +6,35 @@
             </h3>
         </div>
         @if (auth()->user()->role === 'admin' || auth()->user()->role === 'supplier' || auth()->user()->role === 'superAdmin')
-        <div class="m-auto d-flex">
-            <select class="form-select form-control-solid mr-2 @error('user_id') is-invalid @enderror" id="user_id"
-                name="user_id" wire:model.change="userid">
-                <option value="" selected="" disabled="">
-                    Select a user:
-                </option>
+            <div class="m-auto d-flex">
+                <select class="form-select form-control-solid mr-2 @error('user_id') is-invalid @enderror" id="user_id"
+                    name="user_id" wire:model.change="userid">
+                    <option value="" selected="" disabled="">
+                        Select a user:
+                    </option>
 
-                <option value="all">All</option>
-                @foreach ($users as $user)
-                {{-- <option value="{{ $user->id }}" @selected(old('UserId', $userid) == $user->id)> --}}
-                <option value="{{ $user->id }}">
-                    {{ $user->name }}
-                </option>
-                @endforeach
-            </select>
-            <select class="form-select form-control-solid @error('customer_id') is-invalid @enderror"
-                id="customer_id" name="customer_id" wire:model.change="customerid">
-                <option value="" selected="" disabled="">
-                    Select a customer:
-                </option>
+                    <option value="all">All</option>
+                    @foreach ($users as $user)
+                        {{-- <option value="{{ $user->id }}" @selected(old('UserId', $userid) == $user->id)> --}}
+                        <option value="{{ $user->id }}">
+                            {{ $user->name }}
+                        </option>
+                    @endforeach
+                </select>
+                <select class="form-select form-control-solid @error('customer_id') is-invalid @enderror"
+                    id="customer_id" name="customer_id" wire:model.change="customerid">
+                    <option value="" selected="" disabled="">
+                        Select a customer:
+                    </option>
 
-                <option value="all">All</option>
-                @foreach ($customers as $customer)
-                <option value="{{ $customer->id }}">
-                    {{ $customer->name }}
-                </option>
-                @endforeach
-            </select>
-        </div>
+                    <option value="all">All</option>
+                    @foreach ($customers as $customer)
+                        <option value="{{ $customer->id }}">
+                            {{ $customer->name }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
         @endif
         <div class="card-actions d-flex">
 
@@ -93,8 +93,8 @@
                     {{-- <th scope="col" class="align-middle text-center">
                         <a wire:click.prevent="sortBy('invoice_no')" href="#" role="button">
                             {{ __('Invoice No.') }}
-                    @include('inclues._sort-icon', ['field' => 'invoice_no'])
-                    </a>
+                            @include('inclues._sort-icon', ['field' => 'invoice_no'])
+                        </a>
                     </th> --}}
                     <th scope="col" class="align-middle text-center">
                         <a wire:click.prevent="sortBy('user_id')" href="#" role="button">
@@ -127,9 +127,9 @@
                         </a>
                     </th>
                     <th scope="col" class="align-middle text-center">
-                        <a wire:click.prevent="sortBy('note')" href="#" role="button">
-                            {{ __('Note') }}
-                            @include('inclues._sort-icon', ['field' => 'note'])
+                        <a wire:click.prevent="sortBy('payto')" href="#" role="button">
+                            {{ __('Pay To') }}
+                            @include('inclues._sort-icon', ['field' => 'payto'])
                         </a>
                     </th>
                     <th scope="col" class="align-middle text-center">
@@ -151,73 +151,69 @@
             </thead>
             <tbody>
                 @forelse ($orders as $order)
-                <tr>
-                    <td class="align-middle text-center">
-                        {{ $loop->iteration }}
-                    </td>
-                    {{-- <td class="align-middle text-center">
+                    <tr>
+                        <td class="align-middle text-center">
+                            {{ $loop->iteration }}
+                        </td>
+                        {{-- <td class="align-middle text-center">
                             {{ $order->invoice_no }}
-                    </td> --}}
-                    <td class="align-middle text-center">
-                        {{ $order->customer->name }}
-                    </td>
-                    <td class="align-middle text-center">
-                        {{ $order->order_date->format('d-m-Y') }}
-                    </td>
-                    <td class="align-middle text-center">
-                        {{ $order->payment_type }}
-                    </td>
-                    <td class="align-middle text-center">
-                        {{ Number::currency($order->total, 'GBP') }}
-                    </td>
-                    <td class="align-middle text-center">
-                        {{ Number::currency($order->pay, 'GBP') }}
-                    </td>
-                    <td class="align-middle text-center">
-                        @if (auth()->user()->name === 'Adminss')
-                        {{ $order->payto }}
+                        </td> --}}
+                        <td class="align-middle text-center">
+                            {{ $order->customer->name }}
+                        </td>
+                        <td class="align-middle text-center">
+                            {{ $order->order_date->format('d-m-Y') }}
+                        </td>
+                        <td class="align-middle text-center">
+                            {{ $order->payment_type }}
+                        </td>
+                        <td class="align-middle text-center">
+                            {{ Number::currency($order->total, 'GBP') }}
+                        </td>
+                        <td class="align-middle text-center">
+                            {{ Number::currency($order->pay, 'GBP') }}
+                        </td>
+                        <td class="align-middle text-center">
+                            {{ $order->payto }}
+                        </td>
+                        @if (auth()->user()->role === 'admin' || auth()->user()->role === 'supplier' || auth()->user()->role === 'superAdmin')
+                            <td class="align-middle text-center">
+                                {{ $order->user->name }}
+                            </td>
                         @else
-                        {{ $order->note }}
+                            <td class="align-middle text-center">
+                                {{ $order->note }}
+                            </td>
                         @endif
-                    </td>
-                    @if (auth()->user()->role === 'admin' || auth()->user()->role === 'supplier' || auth()->user()->role === 'superAdmin')
-                    <td class="align-middle text-center">
-                        {{ $order->user->name }}
-                    </td>
-                    @else
-                    <td class="align-middle text-center">
-                        {{ $order->note }}
-                    </td>
-                    @endif
-                    <td class="align-middle text-center">
-                        <x-status dot
-                            color="{{ $order->order_status === \App\Enums\OrderStatus::COMPLETE ? 'green' : ($order->order_status === \App\Enums\OrderStatus::PENDING ? 'orange' : '') }}"
-                            class="text-uppercase">
-                            {{ $order->order_status->label() }}
-                        </x-status>
-                    </td>
-                    <td class="align-middle text-center">
-                        <x-button.show class="btn-icon" route="{{ route('orders.show', $order->uuid) }}" />
-                        <x-button.print class="btn-icon" data-bs-toggle="tooltip"
-                            data-bs-original-title="Sale Price"
-                            route="{{ route('order.downloadInvoice', $order->uuid) }}" />
-                        @if (auth()->user()->name === 'Adminss')
-                        <x-button.admin_print class="btn-icon" data-bs-toggle="tooltip"
-                            data-bs-original-title="Cost Price"
-                            route="{{ route('order.downloadAdminInvoice', $order->uuid) }}" />
-                        @endif
-                        @if ($order->order_status === \App\Enums\OrderStatus::PENDING)
-                        <x-button.delete class="btn-icon" route="{{ route('orders.cancel', $order) }}"
-                            onclick="return confirm('Are you sure to cancel invoice no. {{ $order->invoice_no }} ?')" />
-                        @endif
-                    </td>
-                </tr>
+                        <td class="align-middle text-center">
+                            <x-status dot
+                                color="{{ $order->order_status === \App\Enums\OrderStatus::COMPLETE ? 'green' : ($order->order_status === \App\Enums\OrderStatus::PENDING ? 'orange' : '') }}"
+                                class="text-uppercase">
+                                {{ $order->order_status->label() }}
+                            </x-status>
+                        </td>
+                        <td class="align-middle text-center">
+                            <x-button.show class="btn-icon" route="{{ route('orders.show', $order->uuid) }}" />
+                            <x-button.print class="btn-icon" data-bs-toggle="tooltip"
+                                data-bs-original-title="Sale Price"
+                                route="{{ route('order.downloadInvoice', $order->uuid) }}" />
+                            @if (auth()->user()->name === 'Adminss')
+                                <x-button.admin_print class="btn-icon" data-bs-toggle="tooltip"
+                                    data-bs-original-title="Cost Price"
+                                    route="{{ route('order.downloadAdminInvoice', $order->uuid) }}" />
+                            @endif
+                            @if ($order->order_status === \App\Enums\OrderStatus::PENDING)
+                                <x-button.delete class="btn-icon" route="{{ route('orders.cancel', $order) }}"
+                                    onclick="return confirm('Are you sure to cancel invoice no. {{ $order->invoice_no }} ?')" />
+                            @endif
+                        </td>
+                    </tr>
                 @empty
-                <tr>
-                    <td class="align-middle text-center" colspan="8">
-                        No results found
-                    </td>
-                </tr>
+                    <tr>
+                        <td class="align-middle text-center" colspan="8">
+                            No results found
+                        </td>
+                    </tr>
                 @endforelse
                 {{-- @if ($customerid)
                     <tr>
@@ -225,16 +221,16 @@
                             Payed amount
                         </td>
                         <td class="text-center">{{ number_format($orders->sum('pay'), 2) }}</td>
-                </tr>
-                <tr>
-                    <td colspan="8" class="text-end">Due</td>
-                    <td class="text-center">{{ number_format($orders->sum('total') - $orders->sum('pay'), 2) }}
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="8" class="text-end">Total</td>
-                    <td class="text-center">{{ number_format($orders->sum('total'), 2) }}</td>
-                </tr>
+                    </tr>
+                    <tr>
+                        <td colspan="8" class="text-end">Due</td>
+                        <td class="text-center">{{ number_format($orders->sum('total') - $orders->sum('pay'), 2) }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="8" class="text-end">Total</td>
+                        <td class="text-center">{{ number_format($orders->sum('total'), 2) }}</td>
+                    </tr>
                 @endif --}}
             </tbody>
         </table>
