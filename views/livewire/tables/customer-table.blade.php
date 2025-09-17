fadad521a2208a6174339acda5a7d3b6cffeac24
kamran afridi <kamranafridi089@gmail.com>
2025-09-17 02:00:22 +0100

    commit

--- views/livewire/tables/customer-table.blade.php
+++ views/livewire/tables/customer-table.blade.php
@@ -17,7 +17,7 @@
                 Show
                 <div class="mx-2 d-inline-block">
                     <select wire:model.live="perPage" class="form-select form-select-sm" aria-label="result per page">
-                        <option value="5">5</option>
+                        <option value="5">5 </option>
                         <option value="10">10</option>
                         <option value="15">15</option>
                         <option value="25">25</option>
