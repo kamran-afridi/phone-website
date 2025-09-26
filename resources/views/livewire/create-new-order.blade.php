733925ac814baedf4c124b76c90071b22a47f86b
kamran afridi <kamranafridi089@gmail.com>
2025-09-26 02:18:23 +0100

    commit

--- resources/views/livewire/create-new-order.blade.php
+++ resources/views/livewire/create-new-order.blade.php
@@ -156,11 +156,10 @@
                                                                     list="priceOptions-{{ $item->rowId }}"
                                                                     placeholder="Price"
                                                                     autocomplete="off">
-                                                                <select class="form-control" wire:model="cartItemprice.{{ $item->rowId }}">
-                                                                    <option value="{{ $item->options->whole_sale_price }}">Whole Sale Price ({{ $item->options->whole_sale_price }})</option>
-                                                                    <option value="{{ $item->options->sale_price }}">Sale Price ({{ $item->options->sale_price }})</option>
-                                                                </select>
-
+                                                                <datalist id="priceOptions-{{ $item->rowId }}">
+                                                                    <option value="{{ $item->sku['whole_sale_price'] }}">Whole Sale Price</option>
+                                                                    <option value="{{ $item->sku['sale_price'] }}">Sale Price</option>
+                                                                </datalist>
 
                                                             </div>
                                                         </div>
